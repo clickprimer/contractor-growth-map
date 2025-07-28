224e1eea6b12326bd781fa1c1fde2b4f20807406
clickprimer <clickprimer@gmail.com>
2025-07-28 16:45:59 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0068ff',
+        accent: '#2ea3f2',
+        dark: '#002654',
+        cta: '#30d64f',
+        highlight: '#e8cc00',
+        offwhite: '#e8eeff',
+      },
+    },
+  },
+  plugins: [],
+}
